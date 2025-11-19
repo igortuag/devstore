93350afba9ee79c5224103104455ea8b1af68f1d
Igor Tuag <igor@convincely.com>
2025-11-19 08:10:55 -0300

    Refactor code structure for improved readability and maintainability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import rocketseatConfig from "@rocketseat/eslint-config";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...rocketseatConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
-    "next-env.d.ts",
-  ]),
+    "next-env.d.ts"
+  ])
 ]);
 
 export default eslintConfig;
