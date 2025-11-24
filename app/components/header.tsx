406863b07719d9a2be0b33365be9c6d34a429c06
Igor Tuag <igor@convincely.com>
2025-11-24 07:30:15 -0300

    add search form to Header component and update lucide-react dependency

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,24 @@
+import { Search } from "lucide-react";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header>
-      <h1>Store Header</h1>
+    <header className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
+
+        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-9000 px-5 py-3 ring-zinc-700 ">
+          <Search className="w-5 h-5 text-zinc-500" />
+
+          <input
+            placeholder="Find products"
+            className="bg-transparent outline-none flex-1 text-sm placeholder:text-zinc-500 text-white"
+          />
+        </form>
+      </div>
+      <div className="flex items-center gap-4"></div>
     </header>
   );
 }
