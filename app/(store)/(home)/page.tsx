b32f415f6d35742336520a4c019c0e69512c1182
Igor Tuag <igor@convincely.com>
2025-11-27 08:05:54 -0300

    refactor Home component; enhance Link element with Image for improved visual presentation

--- app/(store)/(home)/page.tsx
+++ app/(store)/(home)/page.tsx
@@ -1,9 +1,21 @@
 import { Link } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-3 gap-6">
-      <Link href="/"></Link>
+      <Link
+        href="/"
+        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          alt="Moletom AI Side"
+          width={860}
+          height={860}
+          quality={100}
+        />
+      </Link>
     </div>
   );
 }
