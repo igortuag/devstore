47a896cd828848e767bc2ffd41f116110210ae0c
Igor Tuag <igor@convincely.com>
2025-11-28 08:35:40 -0300

    refactor Home component; enhance Link elements with group styling and improve Image attributes for better visual effects

--- app/(store)/(home)/page.tsx
+++ app/(store)/(home)/page.tsx
@@ -6,13 +6,42 @@ export default function Home() {
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-3 gap-6">
       <Link
         href="/"
-        className="col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+        className="group col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
       >
         <Image
           src="/moletom-ai-side.png"
           alt="Moletom AI Side"
-          width={860}
-          height={860}
+          className="group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/moletom-java.png"
+          className="group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          alt="Moletom Java"
+          width={920}
+          height={920}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="group col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          className="group-hover:scale-105 transition-transform duration-200 ease-in-out"
+          alt="Camiseta Do While 2022"
+          width={920}
+          height={920}
           quality={100}
         />
       </Link>
