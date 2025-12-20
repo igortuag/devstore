4793aa38e31fb5faed4c3f626bedfddd4b60686d
Igor Tuag <igor@convincely.com>
2025-12-20 07:45:32 -0300

    feat: enhance ProductPage to dynamically display product details

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -9,7 +9,9 @@ interface ProductPageProps {
 }
 
 async function getProductBySlug(slug: string): Promise<Product> {
-  const response = await api("product/" + slug);
+  const response = await api(`/product/${slug}`, {
+    next: { revalidate: 60 * 60 }
+  });
   const data = await response.json();
   return data;
 }
@@ -23,8 +25,8 @@ export default async function ProductPage({ params }: ProductPageProps) {
     <div className="relative grid max-h-[860px] grid-cols-3">
       <div className="col-span-2 overflow-hidden">
         <Image
-          src="/moletom-never-stop-learning.png"
-          alt="Sample Product"
+          src={product.image}
+          alt={product.title}
           width={1000}
           height={1000}
           quality={100}
@@ -32,20 +34,29 @@ export default async function ProductPage({ params }: ProductPageProps) {
       </div>
 
       <div className="flex flex-col justify-center px-12">
-        <h1 className="text-3xl font-bold leading-tight">
-          Moletom Never Stop Learning
-        </h1>
+        <h1 className="text-3xl font-bold leading-tight">{product.title}</h1>
 
         <p className="mt-2 leading-relaxed text-zinc-400">
-          Moletom fabricado com 88% algodão orgânico e 12% poliéster reciclado.
+          {product.description}
         </p>
 
         <div className="mt-8 flex items-center gap-3">
           <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-            $129
+            {product.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0
+            })}
           </span>
           <span className="text-sm text-zinc-400">
-            Em 12x s/ juros de $10,75
+            Em 12x s/ juros de{" "}
+            {(product.price / 12).toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+              minimumFractionDigits: 0,
+              maximumFractionDigits: 0
+            })}
           </span>
         </div>
 
