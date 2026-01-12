65697e3c769dc08b7be019d353a0d5d693dab8b0
Igor Tuag <igor@convincely.com>
2026-01-12 06:42:49 -0300

    feat: add test for searching a product and adding it to the cart

--- cypress/e2e/add-a-product-to-cart.cy.ts
+++ cypress/e2e/add-a-product-to-cart.cy.ts
@@ -27,4 +27,22 @@ describe("add a product to cart", () => {
     // cart count should be 1
     cy.contains("Cart (1)").should("exist");
   });
+
+  it("should be able to search for a product and add it to the cart", () => {
+    cy.visit("https://localhost:3000");
+
+    // search for a product
+    cy.get('input[name="q"]').type("moletom").parent("form").submit();
+
+    // click on the first product in the search results
+    cy.get('a[href^="/product"]').first().click();
+
+    cy.url().should("include", "/product");
+
+    // add to cart
+    cy.contains("Add to Cart").click();
+
+    // cart count should be 1
+    cy.contains("Cart (1)").should("exist");
+  });
 });
