a30e0ba4f4e7b0abbfbaaa205a663d18ef82c0eb
Igor Tuag <igortuag@gmail.com>
2026-01-24 07:58:13 -0300

    feat: refactor environment variable handling with zod validation and update dependencies

--- app/env.ts
+++ app/env.ts
@@ -1,15 +1,21 @@
+import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-export const envSchema = z.object({
-  NEXT_PUBLIC_API_BASE_URL: z.string(),
-  APP_URL: z.string()
+export const env = createEnv({
+  server: {
+    APP_URL: z.string().url(),
+  },
+
+
+
+  client: {
+    NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+  },
+
+
+
+  runtimeEnv: {
+    APP_URL: process.env.APP_URL,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
 });
-
-const parsedEnv = envSchema.safeParse(process.env);
-
-if (!parsedEnv.success) {
-  console.error("Invalid environment variables:", parsedEnv.error.format());
-  throw new Error("Invalid environment variables");
-}
-
-export const env = parsedEnv.data;
